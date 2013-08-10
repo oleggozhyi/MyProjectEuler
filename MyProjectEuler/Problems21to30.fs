2040f3566fc2c3874e32709f5dd52a4742c56136
Oleg Gozhyi <oleg.gozhyi@outlook.com>
2013-08-11 00:29:23 +0300

    Solved 21

---
+++ MyProjectEuler/Problems21to30.fs
@@ -0,0 +1,13 @@
+﻿module Problems21to30
+
+let problem21() =
+    let isAmicable n = 
+        let rec divisorsSum n sum i = 
+            if i>(float>>sqrt>>int) n then sum
+            else divisorsSum n (if n%i<>0 then sum else sum+i+n/i) (i+1)
+        let m = 1+divisorsSum n 0 2 in m<>n && 1+divisorsSum m 0 2 = n
+    [1..9999] 
+    |> List.filter isAmicable
+    |> List.sum
+    |> printfn "Problem 21 = %A" //31626
+
--- MyProjectEuler/Program.fs
+++ MyProjectEuler/Program.fs
@@ -2,8 +2,5 @@
 
 [<EntryPoint>]
 let main argv = 
-  let x = System.Diagnostics.Stopwatch.StartNew()
-  Problems61to70.problem67()
-  x.Stop()
-  printfn "%A" x.Elapsed
-  0 
+ Problems11to20.problem14_optimized()
+ 0 
