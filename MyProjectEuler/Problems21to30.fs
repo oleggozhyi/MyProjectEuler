7812e2db67e94a0205a8978f14f876bc39de23df
Oleg Gozhyi <oleg.gozhyi@outlook.com>
2013-08-20 09:59:58 +0300

    Solved 23

--- MyProjectEuler/Problems21to30.fs
+++ MyProjectEuler/Problems21to30.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.IO
+open System.Collections.Generic
 
 let problem21() =
     let isAmicable n = 
@@ -20,4 +21,28 @@ let problem22() =
     let score i (s:string) = (i+1)*Seq.sumBy (fun c->1+(int c)-(int 'A')) s
     names |> Array.sort |> Array.mapi score |> Array.sum
     |> printfn "Problem 22 = %A" //871198282
+    
+let problem23() = 
+    let isAbundant= 
+        let cache = Dictionary<_,_>()
+        let rec divisorsSum n sum i = 
+            if i>(float>>sqrt>>int) n then sum
+            else divisorsSum n (if n%i<>0 then sum elif i*i=n then sum+i else sum+i+n/i) (i+1)
+        fun n-> let cached, value =  cache.TryGetValue n 
+                if cached then value else cache.[n]<-(1+divisorsSum n 0 2 >n); cache.[n]
+    let isNotAbundantSum n = [1..n/2] |> List.tryFind (fun i-> isAbundant i && isAbundant (n-i)) |> (=) None
+    [1..28123] |> List.filter isNotAbundantSum |> List.sum
+    |> printfn "Problem 23 = %A"
+
+
+let problem23_optimized() = 
+    let isAbundant n = 
+        let rec divisorsSum n sum i = 
+            if i>(float>>sqrt>>int) n then sum
+            else divisorsSum n (if n%i<>0 then sum elif i*i=n then sum+i else sum+i+n/i) (i+1)
+        1+divisorsSum n 0 2 >n
+    [1..28123] |> List.filter isAbundant
+    let isNotAbundantSum n = [12..n/2] |> List.tryFind (fun i-> isAbundant i && isAbundant (n-i)) |> (=) None
+    [1..28123] |> List.filter isNotAbundantSum |> List.sum
+    |> printfn "Problem 23 = %A"
     
